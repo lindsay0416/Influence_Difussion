d36225b36067fae97b2008efd5a2e51d183437dd
lindsay0416 <lindsay.z@zhukeke.co.nz>
2023-11-28 18:20:55 +1300

    add git ignore

--- router.py
+++ router.py
@@ -14,29 +14,28 @@ if es.ping():
 else:
     print("Could not connect to Elasticsearch")
 
-
-@app.route('/add_record/<index_name>/<file_name>', methods=['POST'])
-def add_record(index_name, file_name):
-    # index_name = request.json.get('index')
-    # file_name = request.json.get('doc')
+  
+@app.route('/add_record', methods=['POST'])
+def add_record():
+    index_name = request.json.get('index')
+    file_name = request.json.get('file_name')
     document_id = request.json.get('id')
-
     document_body = request.json.get('body')
 
-    if document_id or not document_body:
-        return jsonify({"error": "Index name, document ID, and body are required"}), 400
+    if not document_id or not document_body or not file_name:
+        return jsonify({"error": "Document ID and body are required"}), 400
 
     try:
-        response = es.index(index = index_name, file = file_name, id=document_id, document=document_body)
+        response = es.index(index=index_name, id=document_id, document=document_body)
         return jsonify(response), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
-@app.route('/get_record/<index_name>/<file_name>/<document_id>', methods=['GET'])
-def get_record(index_name, file_name, document_id):
+@app.route('/get_record/<index_name>/<document_id>', methods=['GET'])
+def get_record(index_name, document_id):
     try:
-        response = es.get(index=index_name, file = file_name, id=document_id)
+        response = es.get(index=index_name, id=document_id)
         return jsonify(response['_source']), 200
     except NotFoundError:
         return jsonify({"error": "Document not found"}), 404
