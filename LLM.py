c117d03d8535d99c3ab97951fca812a712ed3df5
lindsay0416 <lindsay.z@zhukeke.co.nz>
2023-12-18 17:59:53 +1300

    LLM

--- LLM.py
+++ LLM.py
@@ -14,17 +14,30 @@ gpt = OpenAI(openai_api_key=api_key)
 
 @app.route('/generate_text', methods=['POST'])
 def generate_text():
-    data = request.get_json()  # Get the JSON data from the request
+    data = request.get_json()
     if not data or 'prompt' not in data:
         return jsonify({"error": "Prompt is required"}), 400
 
     prompt = data['prompt']
-    # Convert prompt to a list as expected by the generate method
     prompt_list = [prompt]
 
     # Generate text using the list of prompts
-    generated_text = gpt.generate(prompt_list)
-    return jsonify({"generated_text": generated_text[0] if generated_text else "No response"})
+    generated_text_result = gpt.generate(prompt_list)
+    # Debug: Print the LLMResult object
+    print("LLMResult object:", generated_text_result)
+    print("Type of LLMResult:", type(generated_text_result))
+
+    # Inspect the attributes of the LLMResult object
+    print("LLMResult attributes:", dir(generated_text_result))
+
+   # Extract the generated text 
+    if generated_text_result and generated_text_result.generations:
+        # Access the first Generation object and get its text
+        generated_text = generated_text_result.generations[0][0].text
+    else:
+        generated_text = "No generation result found."
+
+    return jsonify({"generated_text": generated_text})
 
 if __name__ == '__main__':
     app.run(debug=True)
